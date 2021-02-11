b849951b99bdb9d5d35db45622ab8ec398f8d359
javascriptiser <woronetskianton@gmail.com>
2021-02-11 13:22:25 +0300

    Верстка Header / InputSearch

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux"
 import { InputSearch } from "../InputSearch/InputSearch"
+import style from './style.module.css'
 
 
 export const Header = () => {
 	const currentTitle = useSelector(state => state.sidebarSlice.currentTitle)
-	return <div>
+	return <div className={style.headerWrapper}>
 		<h1>{currentTitle}</h1>
 		<InputSearch />
 	</div>
--- src/Components/InputSearch/InputSearch.js
+++ src/Components/InputSearch/InputSearch.js
@@ -1,13 +1,14 @@
-import { Input, InputLabel, InputAdornment } from '@material-ui/core'
+import { Input, InputLabel, InputAdornment, TextField } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search';
+import style from "./style.module.css";
 
 export const InputSearch = () => {
-	return <div>
-		<Input startAdornment={
-			<InputAdornment position={'start'}>
-				<SearchIcon />
-			</InputAdornment>
-		}
+	return <div className={style.inputWrapper}>
+		<SearchIcon className={style.searchIcon} />
+		<input
+			type="text"
+			className={style.input}
+			placeholder={'Какой магазин вам нужен?'}
 		/>
 	</div>
 }
